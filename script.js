f4c6069f659386abad8ca602d156cbe478ad63ce
joaopedrofonseca <joaopedrofonsec4@gmail.com>
2022-10-14 14:22:39 -0300

    versao-seleção de items

---
+++ script.js
@@ -0,0 +1,77 @@
+function checkFrango(){
+    const checkVerde = document.querySelector(".frango");
+    checkVerde.classList.remove("hide");
+    const desmarcado1 = document.querySelector(".burguer");
+    desmarcado1.classList.add("hide");
+    const desmarcado2 = document.querySelector(".pizza");
+    desmarcado2.classList.add("hide");
+    
+}
+function checkBurguer(){
+    const checkVerde = document.querySelector(".burguer");
+    checkVerde.classList.remove("hide");
+    const desmarcado1 = document.querySelector(".frango");
+    desmarcado1.classList.add("hide");
+    const desmarcado2 = document.querySelector(".pizza");
+    desmarcado2.classList.add("hide");
+
+}
+function checkPizza(){
+    const checkVerde = document.querySelector(".pizza");
+    checkVerde.classList.remove("hide");
+    const desmarcado1 = document.querySelector(".frango");
+    desmarcado1.classList.add("hide");
+    const desmarcado2 = document.querySelector(".burguer");
+    desmarcado2.classList.add("hide");
+}
+function checkCoca(){
+    const checkVerde = document.querySelector(".coca");
+    checkVerde.classList.remove("hide");
+    const desmarcado1 = document.querySelector(".agua");
+    desmarcado1.classList.add("hide");
+    const desmarcado2 = document.querySelector(".cerva");
+    desmarcado2.classList.add("hide");
+}
+function checkAgua(){
+    const checkVerde = document.querySelector(".agua");
+    checkVerde.classList.remove("hide");
+    const desmarcado1 = document.querySelector(".cerva");
+    desmarcado1.classList.add("hide");
+    const desmarcado2 = document.querySelector(".coca");
+    desmarcado2.classList.add("hide");
+}
+function checkCerva(){
+    const checkVerde = document.querySelector(".cerva");
+    checkVerde.classList.remove("hide");
+    const desmarcado1 = document.querySelector(".agua");
+    desmarcado1.classList.add("hide");
+    const desmarcado2 = document.querySelector(".coca");
+    desmarcado2.classList.add("hide");
+}
+function checkPudim(){
+    const checkVerde = document.querySelector(".pudim");
+    checkVerde.classList.remove("hide");
+    const desmarcado1 = document.querySelector(".petit-gateau");
+    desmarcado1.classList.add("hide");
+    const desmarcado2 = document.querySelector(".brownie");
+    desmarcado2.classList.add("hide");
+
+}
+function checkPG(){
+    const checkVerde = document.querySelector(".petit-gateau");
+    checkVerde.classList.remove("hide");
+    const desmarcado1 = document.querySelector(".pudim");
+    desmarcado1.classList.add("hide");
+    const desmarcado2 = document.querySelector(".brownie");
+    desmarcado2.classList.add("hide");
+
+}
+function checkBrownie(){
+    const checkVerde = document.querySelector(".brownie");
+    checkVerde.classList.remove("hide");
+    const desmarcado1 = document.querySelector(".pudim");
+    desmarcado1.classList.add("hide");
+    const desmarcado2 = document.querySelector(".petit-gateau");
+    desmarcado2.classList.add("hide");
+
+}
